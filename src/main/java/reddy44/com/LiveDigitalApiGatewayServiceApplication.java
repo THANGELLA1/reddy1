c918f7c27426763884a3146993ab792a1945df13
Harshitha <Harshitha@DESKTOP-O4RB72F>
2024-02-06 01:11:20 -0800

    hii

---
+++ src/main/java/reddy44/com/LiveDigitalApiGatewayServiceApplication.java
@@ -0,0 +1,14 @@
+package reddy44.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveDigitalApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveDigitalApiGatewayServiceApplication.class, args);
+		System.out.println("done@@@@@");
+	}
+
+}
---
+++ src/test/java/reddy44/com/LiveDigitalApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package reddy44.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveDigitalApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
